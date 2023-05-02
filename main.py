fab4ac77c3db94c51ab5a3d43f3600c1d0649487
tizianoadv <tiziano.nardone.advance@gmail.com>
2023-05-02 15:43:47 +0200

    Sender code

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("hello world")
+import socket
+import time
+import random
+
+server_address = ("10.129.5.220", 8181)
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client_socket.connect(server_address)
+message = "C'est Tiziano, the best"
+client_socket.sendall(message.encode())
+
+client_socket.close()
 
